a3d0477853fda32cd77244ada9cb9e43ed1ead28
owlab-io <contact@owlab.io>
2017-10-18 16:08:33 +0200

    petite correction de lien css sur session 1 css

--- 3_js/0_entrainement_1/app.js
+++ 3_js/0_entrainement_1/app.js
@@ -1,1 +1,8 @@
-console.log("bon appétit !!!");
+
+console.log("welcome to the blair witch project !!!");
+console.log("hello world !!!");
+// console.log(hello);
+console.log("BIIIIIIM");
+console.log(68 + 55);
+console.log({ name: "gui", age: 23});
+console.log(typeof true && false);
