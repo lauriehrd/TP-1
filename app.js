b4248a6a1e09b505529da770d7fedd14665ba12d
laurie <laurie-harmand@hotmail.fr>
2017-10-25 10:47:56 +0200

    premier commit

---
+++ app.js
@@ -0,0 +1,32 @@
+(function () {
+
+	window.onload = function init(){
+
+		function getPageModules(){
+
+
+			var tab = [];
+			var header = document.getElementById("entete");
+			var nav = document.getElementById("search");
+			var aside = document.getElementById("aside");
+			var main = document.getElementById("container");
+			var footer = document.getElementById("footer");
+			tab.push(header, nav, aside, main, footer);
+
+
+		}
+
+		function styliserModule(num, color, bg){
+
+
+
+			styliserModule(tab.length, )
+
+		}
+
+		function getModuleInfos (){
+
+		}
+	}
+
+}());
